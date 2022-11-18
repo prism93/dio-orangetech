0e8575dbebfb81297fda061b34d46758fd648341
prism93 <priscilasemper@hotmail.com>
2022-11-18 14:53:04 -0300

    -

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,27 +1,27 @@
 
+
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon">
-          <span class="number">#001</span>
+          <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
           <div class="detail">
             <ol class="types">
-              <li class="type">grass</li>
-              <li class="type">poison</li>
-            </ol>
-            <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-              alt="${pokemon.name}"
-            />
-          </div>
-        </li>
-    `
+              ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+            </ol >
+            <img src="${pokemon.photo}"
+            alt="${pokemon.name}"
+             />
+          </div >
+        </li >
+  `
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
-PokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newHtml = pokemons.map(convertPokemonToLi).join('')
+  pokemonList.innerHTML = newHtml
 
 })
 
