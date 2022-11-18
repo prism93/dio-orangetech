755ca2565098431d31570091b5bfca1f9751e483
prism93 <priscilasemper@hotmail.com>
2022-11-18 15:54:48 -0300

    -

--- POKEDEX/assets/js/poke-api.js
+++ POKEDEX/assets/js/poke-api.js
@@ -13,15 +13,14 @@ fetch('https://pokeapi.co/api/v2/pokemon')
 */
 
 
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
-    const types = pokeDetail.types.map(typeSlot => typeSlot.type.name)
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
@@ -30,23 +29,22 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
-
 }
 
-pokeApi.getPokemonDetail = (pokemon => {
+pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then(response => response.json())
+        .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
-})
-
-pokeApi.getPokemons = (offset = 0, limit = 151) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then(response => response.json())
-        .then(jsonBody => jsonBody.results)
-        .then(pokemons => pokemons.map(pokeApi.getPokemonDetail))
-        .then(detailRequests => Promise.all(detailRequests))
-        .then(pokemonsDetails => pokemonsDetails)
-        .catch(error => console.log(error))
-
 }
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
+
