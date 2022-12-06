bc9441c328f43879add30798a605345771f39add
prism93 <priscilasemper@hotmail.com>
2022-12-06 11:17:13 -0300

    -

---
+++ dio-react/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import Button from "./components/Button";
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <Button title="entrar" />
+      <Button title="fechar" />
+      <Button title="abrir" />
+      <Button title="salvar" />
+    </div>
+  );
+}
+
+export default App;
