d910444cf72b2b60d37601f6ac05b5f56854c9f7
prism93 <priscilasemper@hotmail.com>
2022-12-12 18:42:20 -0300

    -

--- dio-react/dio/src/pages/login/index.jsx
+++ dio-react/dio/src/pages/login/index.jsx
@@ -1,20 +1,29 @@
+import { MdEmail, MdLock } from 'react-icons/md';
 import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md'
+import { useForm } from "react-hook-form";
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+
 import { api } from '../../services/api';
-
-import { useForm } from "react-hook-form";
-
 
 import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
 
+const schema = yup.object({
+    email: yup.string().email('email não é válido').required('Campo obrigatório'),
+    password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
+  }).required();
+  
+ 
 const Login = () => {
 
     const navigate = useNavigate()
 
     const { control, handleSubmit, formState: { errors  } } = useForm({
+        resolver: yupResolver(schema),
         reValidateMode: 'onChange',
         mode: 'onChange',
     });
@@ -47,9 +56,9 @@ const Login = () => {
                 <TitleLogin>Faça seu cadastro</TitleLogin>
                 <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email" errorMessage={errors?.email?.message}  control={control} />
                     {errors.email && <span>E-mail é obrigatório</span>}
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" errorMessage={errors?.password?.message} control={control}  />
                     {errors.senha && <span>Senha é obrigatório</span>}
                     <Button title="Entrar" variant="secondary" type="submit"/>
                 </form>
